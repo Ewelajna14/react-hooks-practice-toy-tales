1c5dc113cde002963b8297234c4681bf846d35be
Ewelina <ekowalczyk1994@gmail.com>
2021-11-19 16:50:26 -0600

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
@@ -6,19 +6,47 @@ import ToyContainer from "./ToyContainer";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const[toys, setToys]= useState([])
+
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
   }
 
+  useEffect(()=>{
+    fetch("http://localhost:3001/toys")
+    .then( r => r.json())
+    .then( data => setToys(data))
+  }, [])
+
+  function handleCreateForm(newToy){
+    setToys([...toys, newToy ])
+  }
+
+  function handleRemoveButton(deletedToy){
+    const updatedItems = toys.filter((toy) => toy.id !== deletedToy.id);
+  setToys(updatedItems);
+  }
+  
+function handleLikeButton(updatedToy){
+  const updatedToys = toys.map((toy)=>{
+    if(toy.id===updatedToy.id){
+      return updatedToy
+    }else{
+      return toy
+    }
+  })
+  setToys(updatedToys)
+}
+
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
+      {showForm ? <ToyForm onCreateForm ={handleCreateForm}/> : null}
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer toys={toys} onDelete={handleRemoveButton} onLike={handleLikeButton}/>
     </>
   );
 }
--- src/components/ToyForm.js
+++ src/components/ToyForm.js
@@ -1,22 +1,58 @@
-import React from "react";
+import React, {useState} from "react";
 
-function ToyForm() {
+function ToyForm({onCreateForm}) {
+
+const[name, setName]=useState("")
+const[image, setImage]=useState("")
+
+function handleNameChange(event){
+setName(event.target.value)
+}
+
+function handleImageChange(event){
+setImage(event.target.value)
+}
+
+function handleSubmit(event){
+  event.preventDefault()
+  const newToy ={
+    name:name,
+    image:image,
+    likes:0
+  }
+  fetch("http://localhost:3001/toys", {
+    method:"POST",
+    headers:{
+      "Content-Type": "application/json",
+    },
+    body:JSON.stringify(newToy),
+  })
+  .then((r)=> r.json())
+  .then((newToy)=> onCreateForm(newToy))
+
+  setName("")
+  setImage("")
+}
+
+
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form onSubmit={handleSubmit} className="add-toy-form">
         <h3>Create a toy!</h3>
-        <input
+        <input onChange={handleNameChange}
           type="text"
           name="name"
           placeholder="Enter a toy's name..."
           className="input-text"
+          value={name}
         />
         <br />
-        <input
+        <input onChange={handleImageChange}
           type="text"
           name="image"
           placeholder="Enter a toy's image URL..."
           className="input-text"
+          value={image}
         />
         <br />
         <input
